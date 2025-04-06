85360907ff1a1cc7161090211b8b12b93504f25a
pehilbert <148654775+pehilbert@users.noreply.github.com>
2025-04-06 12:37:24 -0700

    Set up navigation and changed names

---
+++ app/src/main/java/com/example/finalproject/ui/DailyFactFragment.kt
@@ -0,0 +1,18 @@
+package com.example.finalproject.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.finalproject.R
+
+class DailyFactFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_daily_fact, container, false)
+    }
+}
